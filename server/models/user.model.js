56a3f9ec970bce0be82895e13778b387445e734c
PyromancerBoom <priyanshmagic@gmail.com>
2022-05-06 15:13:37 +0530

    login route

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,9 +1,10 @@
-import mongoose from "mongoose"
+const { mongoose } = require('mongoose')
 
 const User = new mongoose.Schema(
     {
         name: {type: String, required: true},
-        email: {type: String, required: true, unique:true}, // Unique implies no duplicate value for email
+        email: {type: String, required: true, unique:true},
+        // Unique implies no duplicate value for email
         password: {type: String, required: true},
         phrase: {type: String, required: true},
     },
--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { default: mongoose } = require('mongoose')
+const { mongoose } = require('mongoose')
+const User = require('./models/user.model')
 
 //Middleware(a function) just manipulates the response and usually passes it to the next function
 app.use(cors()) // use the cors middleware to overcome the cors security error
@@ -10,13 +11,36 @@ app.use(express.json()) // Pass anything that comes as body into JSON
 
 mongoose.connect('mongodb://localhost:27017/mern-test-app')
 
-app.get("/",(req,res) => res.send("Response from the GET request"));
+app.get("/", (req, res) => res.send("Response from the GET request"));
 
 // Now we can specify some routes
 // Call it as a function
-app.post('/api/register', (req,res) => {
+app.post('/api/register', async (req, res) => {
     console.log(req.body)
-    res.json({status : 'Ok'})
+    try {
+            await User.create({
+            name: req.body.name,
+            emal: req.body.email,
+            password: req.body.password,
+        })
+        res.json({ status: 'Ok' })
+    } catch (err) {
+        res.json({ status: 'error', error: "Duplicate email" })
+    }
+})
+
+// Login route
+app.post('/api/login', async (req, res) => {
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password,
+    })
+
+    if (user) {
+        return res.json({ status: 'ok', user: true })
+    } else {
+        return res.json({ status: 'error', user: false })
+    }
 })
 
 // Starting a server
