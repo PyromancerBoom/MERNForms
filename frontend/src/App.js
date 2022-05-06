09de963db79d918fa3ba933a7d2777f40546abce
PyromancerBoom <priyanshmagic@gmail.com>
2022-05-06 15:56:30 +0530

    added router, error seems to be in user.model

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
+// import { BrowserRouter, Route } from 'react-router-dom';
 import Register from './Components/Register';
 import Login from './Components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Register />
+    <div className='App'>
       <Login />
+      <Register />
+      {/* <BrowserRouter>
+          {/* http://localhost:3000/login 
+          <Route path='/login' exact component={Login} />
+          <Route path='/register' exact component={Register} />
+      </BrowserRouter> */}
     </div>
   );
 }
 
-export default App;
+export default App
