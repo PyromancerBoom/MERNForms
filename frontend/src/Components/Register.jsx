3377cb2cebb44a36087592b10083bdf74cc46dca
PyromancerBoom <priyanshmagic@gmail.com>
2022-05-06 10:07:39 +0530

    linked to backend

--- frontend/src/Components/Register.jsx
+++ frontend/src/Components/Register.jsx
@@ -4,9 +4,35 @@ const Register = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    /*
+    Async keyword use for define function for 
+    use await task, when you want to use to await for 
+    wait task then first you have to define async function. 
+    Await keyword use for stop execution 
+    till task response, we will use multiple 
+    await calls in one async function.*/
+    async function registerUser(){
+        const response = await fetch('http://localhost:1337/api/register', {
+            // send the data as application json type
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                password,
+            }),
+        })
+
+        // convert to json
+        const data = await response.json()
+        console.log(data)
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={registerUser}>
                 <input
                     value={name}
                     onChange={(e) => setName(e.target.value)}
